902414f9b474d646166779386ce969d211bb8283
Admin <Admin@LAPTOP-U4DN8FG5>
2023-02-22 18:35:56 +0400

    Обновлен класс University

--- UniversityData/UniversityData.Domain/University.cs
+++ UniversityData/UniversityData.Domain/University.cs
@@ -1,19 +1,41 @@
 ﻿
 
 namespace UniversityData.Domain;
-
+/// <summary>
+/// Информация об университете
+/// </summary>
 public class University
 {
+    /// <summary>
+    /// Регистрационный номер
+    /// </summary>
     public string? UniversityNumber { get; set; }
+    /// <summary>
+    /// Название университета
+    /// </summary>
     public string? UniversityName { get; set; }
+    /// <summary>
+    /// Адрес университета
+    /// </summary>
     public string? UniversityAddress { get; set; }
-
-    // RectorData
-
+    /// <summary>
+    /// Сведения о ректоре 
+    /// </summary>
+    public Rector? UniversityRectorData { get; set; }
+    /// <summary>
+    /// Собственность учреждения
+    /// </summary>
     public string? UniversityProperty { get; set; } 
+    /// <summary>
+    /// Собственность здания университета
+    /// </summary>
     public string? UniversityConstructionProperty { get; set; }
-
-    // FacultiesData
-
-    // DepartmentsData
+    /// <summary>
+    /// Факультеты университета
+    /// </summary>
+    public List<Faculty>? UniversityFacultiesData { get; set; } 
+    /// <summary>
+    /// Кафедры университета
+    /// </summary>
+    public List<Department>? UniversityDepartmentsData { get; set;  }
 }

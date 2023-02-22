964e1a67fcd38aca9cb19e05dbb84eb4858a99fd
Admin <Admin@LAPTOP-U4DN8FG5>
2023-02-22 18:06:18 +0400

    Реализован класс University

---
+++ UniversityData/UniversityData.Domain/University.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace UniversityData.Domain;
+
+public class University
+{
+    public string? UniversityNumber { get; set; }
+    public string? UniversityName { get; set; }
+    public string? UniversityAddress { get; set; }
+
+    // RectorData
+
+    public string? UniversityProperty { get; set; } 
+    public string? UniversityConstructionProperty { get; set; }
+
+    // FacultiesData
+
+    // DepartmentsData
+}

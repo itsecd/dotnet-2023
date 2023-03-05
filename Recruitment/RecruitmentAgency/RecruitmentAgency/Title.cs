ff12479438dffed6bf15f3c0478868fb04c77228
YoniqueeZyzzFan <lolvan@mail.ru>
2023-03-05 13:51:37 +0400

    The Title class has been developed

---
+++ Recruitment/RecruitmentAgency/RecruitmentAgency/Title.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentAgency;
+public class Title
+{
+    /// <summary>
+    /// Section - a string that stores section, for example: IT, Finance, etc...
+    /// </summary>  
+    public string Section { set; get; } = string.Empty;
+    /// <summary>
+    /// JobTitle - the string responsible for the title. For example: Programmer, Designer, etc...
+    /// </summary>  
+    public string JobTitle { set; get; } = string.Empty;
+}

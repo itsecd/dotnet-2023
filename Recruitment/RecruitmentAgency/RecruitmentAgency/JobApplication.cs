1a772c7168b368775545101b732ef5ebec29eace
YoniqueeZyzzFan <lolvan@mail.ru>
2023-03-05 13:51:51 +0400

    The JobApplication class has been developed

---
+++ Recruitment/RecruitmentAgency/RecruitmentAgency/JobApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentAgency;
+public class JobApplication{
+    /// <summary>
+    /// Employee - contains information about the employee
+    /// </summary>  
+    public Employee Employee { get; set; } = new();
+    /// <summary>
+    /// Date - date of application
+    /// </summary>  
+    public DateTime Date { set; get; } = DateTime.MinValue;
+    /// <summary>
+    /// Title - responsible for the job title
+    /// </summary>
+    public Title Title { set; get; } = new();
+}

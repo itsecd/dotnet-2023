a0c8f7511ede7974760986a969ababcccc6ba6b0
YoniqueeZyzzFan <lolvan@mail.ru>
2023-03-05 13:52:15 +0400

    The CompanyApplication class has been developed

---
+++ Recruitment/RecruitmentAgency/RecruitmentAgency/CompanyApplication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentAgency;
+public class CompanyApplication{
+    /// <summary>
+    /// Company - contains information about the company
+    /// </summary>  
+    public Company Company { get; set; } = new();
+    /// <summary>
+    /// Date - date of application
+    /// </summary>  
+    public DateTime Date { set; get; } = DateTime.MinValue;
+    /// <summary>
+    /// WorkExperience - shows the required work experience in years
+    /// </summary>
+    public int WorkExperience { set; get; } = 0;
+    /// <summary>
+    /// Salary - shows the wages which the company will pay
+    /// </summary>
+    public int Salary { set; get; } = 0;
+    /// <summary>
+    /// Title - shows the wages which the company will pay
+    /// </summary>
+    public Title Title{ set; get; } = new();
+}

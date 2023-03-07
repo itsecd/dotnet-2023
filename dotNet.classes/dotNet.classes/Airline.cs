79ae790e750baf26be797e1e0dfa3fd659bffb0d
Данил Егориков <dveav@yandex.ru>
2023-03-08 00:55:42 +0400

    edit Airline class

--- dotNet.classes/dotNet.classes/Airline.cs
+++ dotNet.classes/dotNet.classes/Airline.cs
@@ -10,15 +10,15 @@ public class Airline
     /// <summary>
     /// List airplanes in airline
     /// </summary>
-    public List<Airplane> Airplanes { get; set; }
+    public List<Airplane> Airplanes { get; set; } = new List<Airplane>();
     /// <summary>
     /// List flights in airline
     /// </summary>
-    public List<Flight> Flights { get; set; }
+    public List<Flight> Flights { get; set; } = new List<Flight>();
     /// <summary>
     /// List passengers in airline
     /// </summary>
-    public List<Passenger> Passengers { get; set; }
+    public List<Passenger> Passengers { get; set; } = new List<Passenger>();
 
     public Airline() {}
     public Airline(List<Airplane> airplanes, List<Flight> flights, List<Passenger> passengers)
@@ -26,6 +26,39 @@ public class Airline
         Airplanes = airplanes;
         Flights = flights;
         Passengers = passengers;
+    }    
+
+    /// <summary>
+    /// Method for adding airplane in airline
+    /// </summary>
+    /// <param name="airplane">
+    /// airplane
+    /// </param>
+    public void Add_to_airplane_list(Airplane airplane)
+    {
+        Airplanes.Add(airplane);
+    }
+
+    /// <summary>
+    /// Method for adding flight in airline
+    /// </summary>
+    /// <param name="flight">
+    /// flight
+    /// </param>
+    public void Add_to_flight_list(Flight flight)
+    {
+        Flights.Add(flight);
+    }
+
+    /// <summary>
+    /// Method for adding passenger in airline
+    /// </summary>
+    /// <param name="passenger">
+    /// passenger
+    /// </param>
+    public void Add_to_passenger_list(Passenger passenger)
+    {
+        Passengers.Add(passenger);
     }
 
 }

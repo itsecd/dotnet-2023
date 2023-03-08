3868d6c1310edac29e4a6f5a8a40daee9e3c86f1
artiom.danilov@outlook.com <artiom.danilov@outlook.com>
2023-03-08 14:25:39 +0400

    Исправлен один запрос, добавлены дополнительные проверки в тестах

--- Non-residentialFund/Non-ResidentialFundTests/Non-residentialFundTests.cs
+++ Non-residentialFund/Non-ResidentialFundTests/Non-residentialFundTests.cs
@@ -1,4 +1,5 @@
 ﻿using Non_residentialFundDomain;
+using System;
 
 namespace Non_ResidentialFundTests;
 public class Non_residentialFundTests
@@ -15,6 +16,14 @@ public class Non_residentialFundTests
         var buyers = (from buyer in fictureBuyers select buyer).ToList();
         
         Assert.Equal(8, fictureBuyers.Count);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 1);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 2);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 3);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 4);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 5);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 6);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 7);
+        Assert.Contains(buyers, buyer => buyer.BuyerId == 8);
     }
 
     /// <summary>
@@ -32,10 +41,12 @@ public class Non_residentialFundTests
                                                     group buildingAuction by buildingAuction.AuctionId into buildingsInAuction
                                                     select new { AuctionId = buildingsInAuction.First().AuctionId, countTrySale = buildingsInAuction.Count() })
                                                    on auction.AuctionId equals countTrySaleInAuction.AuctionId
-                     where countBoughtInAuction.countBought == countTrySaleInAuction.countTrySale
+                     where countBoughtInAuction.countBought != countTrySaleInAuction.countTrySale
                      select auction.AuctionId).ToList();
         
-        Assert.Equal(4, result.Count);
+        Assert.Equal(2, result.Count);
+        Assert.Contains(result, auctionId => auctionId == 1);
+        Assert.Contains(result, auctionId => auctionId == 5);
     }
 
     /// <summary>
@@ -48,11 +59,22 @@ public class Non_residentialFundTests
         var result = (from buyer in _fixture.FixtureBuyers
                      join privatized in _fixture.FixturePrivatized on buyer.BuyerId equals privatized.BuyerId
                      join building in _fixture.FixtureBuildings on privatized.RegistrationNumber equals building.RegistrationNumber
+                     join districtCountSold in ( from building in _fixture.FixtureBuildings
+                                                 join privatized in _fixture.FixturePrivatized on building.RegistrationNumber equals privatized.RegistrationNumber
+                                                 group new { privatized, building } by building.DistrictId into privGroupByDistrict
+                                                 select new { DistrictId =  privGroupByDistrict.First().building.DistrictId, 
+                                                     CountSold = privGroupByDistrict.Count()}
+                     ).ToList() on building.DistrictId equals districtCountSold.DistrictId
                      where building.DistrictId == 1
                      orderby buyer.LastName, buyer.FirstName
-                     select new {buyer.BuyerId, buyer.LastName, buyer.FirstName, buyer.MiddleName}).ToList();
+                     select new {buyer.BuyerId, buyer.LastName, buyer.FirstName, buyer.MiddleName, districtCountSold.CountSold}).ToList();
         
         Assert.Equal(3, result.Count);
+        Assert.Equal(3, result.First().CountSold);
+        Assert.Equal("Аскерова", result.First().LastName);
+        Assert.Contains(result, buyer => buyer.BuyerId == 1);
+        Assert.Contains(result, buyer => buyer.BuyerId == 4);
+        Assert.Contains(result, buyer => buyer.BuyerId == 8);
     }
 
     /// <summary>
@@ -64,10 +86,15 @@ public class Non_residentialFundTests
         var result = (from buyerAuction in _fixture.FixtureBuyerAuctionConnections
                      join auction in _fixture.FixtureAuctions on buyerAuction.AuctionId equals auction.AuctionId
                      join buyer in _fixture.FixtureBuyers on buyerAuction.BuyerId equals buyer.BuyerId
-                     where auction.Date == new DateOnly(2022, 3, 20)
+                     where auction.Date == new DateOnly(2022, 3, 21)
                      select new { buyer.BuyerId, buyer.Address, buyer.LastName, buyer.FirstName }).ToList();
 
-        Assert.Equal(8, result.Count);
+        Assert.Equal(5, result.Count);
+        Assert.Contains(result, buyer => buyer.BuyerId == 1);
+        Assert.Contains(result, buyer => buyer.BuyerId == 3);
+        Assert.Contains(result, buyer => buyer.BuyerId == 4);
+        Assert.Contains(result, buyer => buyer.BuyerId == 7);
+        Assert.Contains(result, buyer => buyer.BuyerId == 8);
     }
 
     /// <summary>
@@ -83,6 +110,8 @@ public class Non_residentialFundTests
                       select new { BuyerId = privGRoup.First().BuyerId, expenses = privGRoup.Sum(privatized => privatized.EndPrice) }).Take(5).ToList();
         
         Assert.Equal(4, result.Count);
+        Assert.Equal(8, result.First().BuyerId);
+        Assert.Equal(19028350.17, result.First().expenses);
     }
 
     /// <summary>
@@ -99,5 +128,9 @@ public class Non_residentialFundTests
                      privGRoup.Sum(privatized => privatized.EndPrice - privatized.StartPrice) }).ToList();
         
         Assert.Equal(6, result.Count);
+        Assert.Equal(13239763.17, result[0].income);
+        Assert.Equal(3, result[0].AuctionId);
+        Assert.Equal(2069090.07, result[1].income, 0.01);
+        Assert.Equal(7, result[1].AuctionId);
     }
 }
--- Non-residentialFund/Non-residentialFundDomain/Privatized.cs
+++ Non-residentialFund/Non-residentialFundDomain/Privatized.cs
@@ -13,11 +13,20 @@ public class Privatized
     /// </summary>
     public int BuyerId { get; set; } = 0;
     /// <summary>
-    /// 
+    /// AuctionId - The id of the auction at which the building was sold
     /// </summary>
     public int AuctionId { get; set; } = 0;
+    /// <summary>
+    /// SaleDate - Date of sale of the building 
+    /// </summary>
     public DateOnly SaleDate { get; set; } = new DateOnly();
+    /// <summary>
+    /// StartPrice - The starting price for this building at the auction
+    /// </summary>
     public double StartPrice { get; set; } = 0.0;
+    /// <summary>
+    /// EndPrice - The final price of this building at the auction
+    /// </summary>
     public double EndPrice { get; set; } = 0.0;
     public Privatized(int registrationNumber, int buyerId, int auctionId, DateOnly saleDate, double startPrice, double endPrice)
     {

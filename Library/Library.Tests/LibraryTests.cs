93a10928c325af0a825705f5b605b1bb22ef3d56
Влад Черный <stalkervaultboy@gmail.com>
2023-03-09 21:46:58 +0400

    Added first test (request)

--- Library/Library.Tests/LibraryTests.cs
+++ Library/Library.Tests/LibraryTests.cs
@@ -1,10 +1,25 @@
 namespace Library.Tests;
 
-public class LibraryTests
-{
-    [Fact]
-    public void Test1()
-    {
+using System.Linq;
 
+public class LibraryTests : IClassFixture<LibraryFixture>
+{
+    private readonly LibraryFixture _fixture;
+
+    public LibraryTests(LibraryFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    /// <summary>
+    /// First request - give info about book by the cipher
+    /// </summary>
+    [Fact]
+    public void CipherTest()
+    {
+        var fixtureBook = _fixture.FixtureBook;
+        var request = (from book in fixtureBook
+                       where book.Cipher == "5698/197b"
+                       select book).Count();
+        Assert.Equal(1, request);
     }
 }

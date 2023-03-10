b5b023eddc99c716f2dd8dc2219ef9115bd775b7
Стефан Степанов <a1806430@yandex.ru>
2023-03-10 19:37:51 +0400

    .

--- Fabric.Domain/Fabric.Test/FabricTests.cs
+++ Fabric.Domain/Fabric.Test/FabricTests.cs
@@ -73,7 +73,6 @@ public class FabricTests : IClassFixture<FabricFixture>
                           Form = g.Key,
                           Count = g.Count()
                       };
-
         Assert.Equal(2, request.First(x => x.Form == "рнн").Count);
     }
     /// <summary>

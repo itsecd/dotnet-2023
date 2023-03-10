5d89c4fd58ffcdab7948b178f6cbb431c4325156
gwymlas <smrszr63@gmail.com>
2023-03-10 14:03:30 +0400

    Добавлены тесты для 5 и 6 запроса

--- TaxiApp/Taxi.Tests/TaxiTests.cs
+++ TaxiApp/Taxi.Tests/TaxiTests.cs
@@ -1,6 +1,7 @@
 using Taxi.Domain;
 using System.Linq;
 using System.Threading.Channels;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Xunit.Abstractions;
 
 namespace Taxi.Tests;
@@ -139,7 +140,7 @@ public class TaxiTests : IClassFixture<TaxiFixture>
             }).Take(2).ToList();
 
         Assert.Contains(query, elem => elem.Count == 4);
-        Assert.DoesNotContain(query, elem => elem.Count == 5 || elem.Count == 3);
+        Assert.DoesNotContain(query, elem => elem.Count == 3);
         
         foreach (var elem in query)
         {
@@ -158,7 +159,78 @@ public class TaxiTests : IClassFixture<TaxiFixture>
         {
             vehicles[(int)ride.VehicleId - 1].Rides.Add(ride);
         }
+
+        var query = (from ridesInfo in (from r in rides
+                group r by r.VehicleId
+                into grp
+                select new
+                {
+                    vehicleId = grp.Key,
+                    count = grp.Count(),
+                    avg = grp.Average(ride =>
+                        ride.RideTime.Second + 60 * ride.RideTime.Minute + 3600 * ride.RideTime.Hour),
+                    max = grp.Max(ride => ride.RideTime.Second + 60 * ride.RideTime.Minute + 3600 * ride.RideTime.Hour)
+                })
+            from d in drivers
+            where d.Id == ridesInfo.vehicleId
+            select new
+            {
+                d.LastName,
+                d.FirstName,
+                d.Patronymic,
+                ridesInfo.count,
+                ridesInfo.avg,
+                ridesInfo.max
+            }).ToList();
         
-        
+        Assert.Equal(5, query.Count());
+        Assert.Equal(1980, query[4].avg);
+        Assert.Equal(2400, query[0].max);
+        Assert.Equal(4, query[2].count);
+
+        foreach (var elem in query)
+        {
+            _testOutputHelper.WriteLine(elem.ToString());
+        }
     }
+    
+    [Fact]
+    public void MaxRidesOfPassenger()
+    {
+        var passengers = _fixture.FixturePassengers;
+        var rides = _fixture.FixtureRides;
+        foreach (var ride in rides)
+        {
+            passengers[(int)ride.PassengerId - 1].Rides.Add(ride);
+        }
+
+        var minDate = new DateTime(2023, 2, 3);
+        var maxDate = new DateTime(2023, 2, 6);
+
+        var subquery = (from p in passengers
+            from r in p.Rides
+            where r.RideDate < maxDate && r.RideDate > minDate
+            group p.Id by p
+            into grp
+            select new
+            {
+                grp.Key.LastName,
+                grp.Key.FirstName,
+                grp.Key.Patronymic,
+                count = grp.Count()
+
+            });
+
+        var max = subquery.Max(elem => elem.count);
+        
+        var query = (from sq in subquery
+            where sq.count == max
+            select sq).ToList();
+        
+        foreach (var elem in query)
+        {
+            _testOutputHelper.WriteLine(elem.ToString());
+        }
+    }
+    
 }

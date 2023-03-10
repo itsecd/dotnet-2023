1ef509bad983ca16b292f4e4cedd95058d3cf63b
Andrey Zero <notqwerty1337@gmail.com>
2023-03-10 16:02:13 +0400

    Added necessary classes

--- Warehouse/WareHouse/CompanyWarehouse.cs
+++ Warehouse/WareHouse/CompanyWarehouse.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Warehouse
+﻿namespace Warehouse;
+/// <summary>
+/// Warehouse - a class that describes the company supply with goods
+/// </summary>
+public class CompanyWarehouse
 {
-    internal class Class1
-    {
-    }
+    /// <summary>
+    /// Cells - show product cell number 
+    /// </summary>  
+    public string Cells { set; get; } = string.Empty;
+    /// <summary>
+    /// Count - shows amount of product
+    /// </summary>
+    public List<Goods> Count { set; get; } = new List<Goods>();
+    /// <summary>  
+    /// ID - shows the product's id
+    /// </summary>  
+    public List<Goods> ID { set; get; } = new List<Goods>();
+    /// <summary><
+    /// Name - a string that stores product name 
+    /// </summary>
+    public List<Goods> Name { set; get; } = new List<Goods>();
+    /// <summary><
+    /// CompanyName - a string that stores company name what get supply
+    /// </summary>
+    public List<Goods> CompanyName { set; get; } = new List<Goods>();
 }
--- Warehouse/WareHouse/Supply.cs
+++ Warehouse/WareHouse/Supply.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Warehouse
+﻿namespace Warehouse;
+/// <summary>
+/// Warehouse - a class that describes the company supply with goods
+/// </summary>
+public class Supply
 {
-    internal class Class1
-    {
-    }
+    /// <summary>
+    /// Count - shows amount of product
+    /// </summary>  
+    public int Count { set; get; }
+    /// <summary>
+    /// CompanyName - contain string of company what get supply
+    /// </summary>
+    public List<Supply> CompanyName { set; get; } = new List<Supply>();
+    /// <summary>  
+    /// ID - shows the product's id
+    /// </summary>  
+    public int ID { set; get; }
+    /// <summary><
+    /// Name - a string that stores company name 
+    /// </summary>
+    public string Name { set; get; } = string.Empty;
 }

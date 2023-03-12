c1c97b4d4953ddf65200180ead016e168d0ff597
dim_nice@mail.com <dim_nice@mail.com>
2023-03-12 21:41:03 +0300

    create classes ticket, client, flight and airplane

---
+++ AirplaneBookingSystem.Domain/AirplaneBookingSystem.Domain/Client.cs
@@ -0,0 +1,36 @@
+﻿namespace AirplaneBookingSystem.Domain;
+
+/// <summary>
+/// Сlass describing the client
+/// </summary>
+public class Client
+{
+    /// <summary>
+    /// Unique Id of client
+    /// </summary>
+    public int Id { get; set; } = 0;
+    /// <summary>
+    /// Passport number 
+    /// </summary>
+    public int PassportNumber { get; set; } = 0;
+    /// <summary>
+    /// Client`s birthday
+    /// </summary>
+    public DateOnly BirthdayData { get; set; } = new DateOnly();
+    /// <summary>
+    /// Client`s name
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// Represent a tickets 
+    /// </summary>
+    public List<Ticket> Tickets { get; set; } = new List<Ticket>();
+    public Client() { }
+    public Client(int passportNumber, DateOnly birthdayData, string name)
+    {
+        PassportNumber = passportNumber;
+        BirthdayData = birthdayData;
+        Name = name;
+        Tickets = new List<Ticket>();
+    }
+}
---
+++ AirplaneBookingSystem.Domain/AirplaneBookingSystem.Domain/Flight.cs
@@ -0,0 +1,46 @@
+﻿namespace AirplaneBookingSystem.Domain;
+
+/// <summary>
+/// Сlass describing the flight of an airplane
+/// </summary>
+public class Flight
+{
+    /// <summary>
+    /// Unique Id of flight
+    /// </summary>
+    public int Id { get; set; } = 0;
+    /// <summary>
+    /// Number of flight
+    /// </summary>
+    public int NumberOfFlight { get; set; } = 0;
+    /// <summary>
+    /// Departure city
+    /// </summary>
+    public string DepartureCity { get; set; } = string.Empty;
+    /// <summary>
+    /// Arrival city
+    /// </summary>
+    public string ArrivalCity { get; set; } = string.Empty;
+    /// <summary>
+    /// Departure date
+    /// </summary>
+    public DateOnly DepartureDate { get; set; } = new DateOnly();
+    /// <summary>
+    /// Arrival date
+    /// </summary>
+    public DateOnly ArrivalDate { get; set; } = new DateOnly();
+    /// <summary>
+    /// List of tickets on this flight
+    /// </summary>
+    public List<Ticket> Tickets { get; set; } = new List<Ticket> { };
+    public Flight() { }
+    public Flight(int numberOfFlight, string departureCity, string arrivalCity, DateOnly departureDate, DateOnly arrivalDate)
+    {
+        NumberOfFlight = numberOfFlight;
+        DepartureCity = departureCity;
+        ArrivalCity = arrivalCity;
+        DepartureDate = departureDate;
+        ArrivalDate = arrivalDate;
+        Tickets = new List<Ticket>();
+    }
+}

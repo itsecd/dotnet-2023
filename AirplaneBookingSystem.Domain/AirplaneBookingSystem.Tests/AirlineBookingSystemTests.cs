9b15b81b10751563ef30e35b551afe0039e3841d
dim_nice@mail.com <dim_nice@mail.com>
2023-03-13 00:29:16 +0300

    first test

---
+++ AirplaneBookingSystem.Domain/AirplaneBookingSystem.Tests/AirlineBookingSystemTests.cs
@@ -0,0 +1,17 @@
+﻿namespace AirplaneBookingSystem.Tests;
+
+public class ClassesTest : IClassFixture<AirlineBookingSystemFixture>
+{
+    private readonly AirlineBookingSystemFixture _fixture;
+    public ClassesTest(AirlineBookingSystemFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    [Fact]
+    public void AllFlights()
+    {
+        var request = (from flight in _fixture.FixtureFlights
+                       select flight).Count();
+        Assert.Equal(5, request);
+    }
+}

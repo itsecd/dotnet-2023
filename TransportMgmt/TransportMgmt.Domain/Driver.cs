850c69d0a54266827bfa9c0e99720f5885d3e7cc
Степан Водянов <2020-01161@students.ssau.ru>
2023-03-21 09:47:28 +0400

    Added driver class and its fields

---
+++ TransportMgmt/TransportMgmt.Domain/Driver.cs
@@ -0,0 +1,34 @@
+﻿namespace TransportMgmt.Domain;
+
+public class Driver
+{
+    public int DriverId { get; set; } = 0;
+
+    public string FistName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string MidleName { get; set; } = string.Empty;
+
+    public int Passport { get; set; } = 0;
+
+    public int DriverLicense { get; set; } = 0;
+
+    public string Address { get; set; } = string.Empty;
+
+    public int PhoneNumber { get; set; } = 0;
+
+    public Driver() { }
+
+    public Driver(int driverId, string fistName, string lastName, string midleName, int passport, int driverLicense, string address, int phoneNumber)
+    {
+        DriverId = driverId;
+        FistName = fistName;
+        LastName = lastName;
+        MidleName = midleName;
+        Passport = passport;
+        DriverLicense = driverLicense;
+        Address = address;
+        PhoneNumber = phoneNumber;
+    }
+}

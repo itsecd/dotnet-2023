d0ec22117cae05e838aee9c624fb012131cf86f7
Степан Водянов <2020-01161@students.ssau.ru>
2023-03-22 23:43:43 +0400

    Added description of TransportType class and his fields.

--- TransportMgmt/TransportMgmt.Domain/TransportType.cs
+++ TransportMgmt/TransportMgmt.Domain/TransportType.cs
@@ -1,8 +1,15 @@
 ﻿namespace TransportMgmt.Domain;
-
+/// <summary>
+/// Class TransportType is used to store information about type of transport
+/// </summary>
 public class TransportType
 {
+    /// <summary>
+    /// Unique key of transport type
+    /// </summary>
     public int TypeId { get; set; }
-
+    /// <summary>
+    /// Name type of trasport
+    /// </summary>
     public string TypeName { get; set; } = string.Empty;
 }

df29943d9593459859f6c5264085da8154e8d6d2
Admin <Admin@LAPTOP-U4DN8FG5>
2023-03-26 03:29:40 +0400

    Реализованы классы *PostDto

---
+++ UniversityData/UniversityData.Server/Dto/SpecialtyTableNodePostDto.cs
@@ -0,0 +1,17 @@
+﻿namespace UniversityData.Server.Dto;
+
+public class SpecialtyTableNodePostDto
+{
+    /// <summary>
+    /// ID специальности
+    /// </summary>
+    public int SpecialtyID { get; set; }
+    /// <summary>
+    /// Количество групп
+    /// </summary>
+    public int CountGroups { get; set; }
+    /// <summary>
+    /// ID университета
+    /// </summary>
+    public int UniversityId { get; set; }
+}
---
+++ UniversityData/UniversityData.Server/Dto/UniversityPostDto.cs
@@ -0,0 +1,29 @@
+﻿namespace UniversityData.Server.Dto;
+
+public class UniversityPostDto
+{
+    /// <summary>
+    /// Регистрационный номер
+    /// </summary>
+    public string Number { get; set; }
+    /// <summary>
+    /// Название университета
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// Адрес университета
+    /// </summary>
+    public string Address { get; set; }
+    /// <summary>
+    /// ID ректора
+    /// </summary>
+    public int RectorId { get; set; }
+    /// <summary>
+    /// Собственность учреждения
+    /// </summary>
+    public string UniversityProperty { get; set; }
+    /// <summary>
+    /// Собственность здания университета
+    /// </summary>
+    public string ConstructionProperty { get; set; }
+}

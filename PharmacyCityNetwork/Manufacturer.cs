7aed1625c42d221139e4f7589008d84c1a6a0568
Анастасия Пиженко <apizhenko02@mail.ru>
2023-03-29 23:21:24 +0400

    created manufacture

---
+++ PharmacyCityNetwork/Manufacturer.cs
@@ -0,0 +1,23 @@
+﻿namespace PharmacyCityNetwork
+{
+    /// <summary>
+    /// Сlass describing an Manufacturer
+    /// </summary>
+    public class Manufacturer
+    {
+        /// <summary>
+        /// Unique Id of Manufacturer
+        /// </summary>
+        public int ManufacturerId { get; set; } = 0;
+        /// <summary>
+        /// Manufacturer Name
+        /// </summary>
+        public string ManufacturerName { get; set; } = string.Empty;
+
+        public Manufacturer() { }
+        public Manufacturer(string ManufacturerName)
+        {
+            this.ManufacturerName = ManufacturerName;
+        }
+    }
+}

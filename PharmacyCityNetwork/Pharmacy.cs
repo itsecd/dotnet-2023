b18f9994518ed7ba6257380cb30ee70ac7be49fa
Анастасия Пиженко <apizhenko02@mail.ru>
2023-03-30 15:02:39 +0400

    created pharmacy

---
+++ PharmacyCityNetwork/Pharmacy.cs
@@ -0,0 +1,30 @@
+﻿namespace PharmacyCityNetwork
+{
+    /// <summary>
+    /// Сlass describing an Pharmancy
+    /// </summary>
+    public class Pharmacy
+    {
+        /// <summary>
+        /// Unique Id of Pharmacy
+        /// </summary>
+        public int PharmacyId { get; set; } = 0;
+        /// <summary>
+        /// Pharmacy Name
+        /// </summary>
+        public string PharmacyName { get; set; } = string.Empty;
+        /// <summary>
+        /// Pharmacy Phone
+        /// </summary>
+        public string PharmacyPhone { get; set; } = string.Empty;
+        /// <summary>
+        /// Pharmacy Address
+        /// </summary>
+        public string PharmacyAddress { get; set; } = string.Empty;
+        /// <summary>
+        /// Pharmacy Director
+        /// </summary>
+      
+
+    }
+}

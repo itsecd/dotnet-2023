d7f8b6b3f170539911e7dd6c3a710da7dd79b9e0
Анастасия Пиженко <apizhenko02@mail.ru>
2023-03-30 15:06:00 +0400

    created pharmagroup

---
+++ PharmacyCityNetwork/PharmaGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace PharmacyCityNetwork
+{
+    /// <summary>
+    /// Сlass describing an pharma group
+    /// </summary>
+    public class PharmaGroup
+    {
+        /// <summary>
+        /// Unique Id of Pharma Group
+        /// </summary>
+        public int PharmaGroupId { get; set; } = 0;
+        /// <summary>
+        /// Pharma Group Name
+        /// </summary>
+        public string PharmaGroupName { get; set; } = string.Empty;
+        
+    }
+}

b2d17191102f1973bcc1e89199b484cbd39d41e2
YoniqueeZyzzFan <lolvan@mail.ru>
2023-04-02 18:08:16 +0400

    Remove unnecessary logic

--- Recruitment/RecruitmentAgency/IntegrationTests/IntegrationTests.cs
+++ Recruitment/RecruitmentAgency/IntegrationTests/IntegrationTests.cs
@@ -7,7 +7,6 @@ namespace MyServer.Tests;
 public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
-
     public IntegrationTests(WebApplicationFactory<Program> factory)
     {
         _factory = factory;
@@ -16,16 +15,11 @@ public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
     [Fact]
     public async Task Get_Values_ReturnsSuccess()
     {
-        // Arrange
-        var client = _factory.CreateClient(); //https://localhost:7144/
+        var client = _factory.CreateClient();
 
-        // Act
         var response = await client.GetAsync("api/Company");
 
-        // Assert
-        response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-        var companies = JsonConvert.DeserializeObject<List<Company>>(content);
+        var companies = JsonConvert.DeserializeObject<List<Company>>(response);
         Assert.Equal(3, companies.Count());
     }
 }

15b50e396967f6546e797168be4897dd54fb6513
Анастасия Пиженко <apizhenko02@mail.ru>
2023-04-10 13:13:16 +0400

    created product

---
+++ PharmacyCityNetwork/PharmacyCityNetwork.Module/Product.cs
@@ -0,0 +1,33 @@
+﻿namespace PharmacyCityNetwork;
+   
+    /// <summary>
+     /// Сlass describing an Product
+     /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Unique Id of Product
+        /// </summary>
+        public int ProductId { get; set; } = 0;
+        /// <summary>
+        /// Product Name
+        /// </summary>
+        public string ProductName { get; set; } = string.Empty;
+        /// <summary>
+        /// Product Group
+        /// </summary>
+        public Group Group { get; set; } = new();
+        /// <summary>
+        /// Product Manufacturer
+        /// </summary>
+        public Manufacturer Manufacturer { get; set; } = new();
+        /// <summary>
+        /// Pharma Group
+        /// </summary>
+        public List<PharmaGroup> PharmaGroups { get; set; } = new();   
+        public Product() { }
+        public Product(string ProductName)
+        {
+            this.ProductName = ProductName;
+        }
+    }

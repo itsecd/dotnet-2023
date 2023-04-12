3e00ca502b50985863fcf210b20be8a7c181b316
gudzen2517 <119973938+gudzen2517@users.noreply.github.com>
2023-04-12 14:06:57 +0300

    Update MappingProfile.cs

--- TransportManagment/TransportManagment.Server/MappingProfile.cs
+++ TransportManagment/TransportManagment.Server/MappingProfile.cs
@@ -1,8 +1,22 @@
-﻿using AutoMapper;using TransportManagment.Classes;using TransportManagment.Server.Dto;namespace TransportManagment.Server;
+using AutoMapper;
+using TransportManagment.Classes;
+using TransportManagment.Server.Dto;
+namespace TransportManagment.Server;
 /// <summary>
 /// Class for AutoMapper
-/// </summary>public class MappingProfile : Profile{
+/// </summary>
+public class MappingProfile : Profile
+{
     /// <summary>
     /// Constructor
-    /// </summary>    public MappingProfile()     {        CreateMap<Classes.Route, RouteGetDto>();        CreateMap<RoutePostDto, Classes.Route>();        CreateMap<Transport, TransportGetDto>();        CreateMap<TransportPostDto, Transport>();
-        CreateMap<Driver, DriverGetDto>();        CreateMap<DriverPostDto, Driver>();    }}
+    /// </summary>
+    public MappingProfile() 
+    {
+        CreateMap<Classes.Route, RouteGetDto>();
+        CreateMap<RoutePostDto, Classes.Route>();
+        CreateMap<Transport, TransportGetDto>();
+        CreateMap<TransportPostDto, Transport>();
+        CreateMap<Driver, DriverGetDto>();
+        CreateMap<DriverPostDto, Driver>();
+    }
+}

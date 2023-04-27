9037a877c7023477270700d5b20195447f52ec2c
YoniqueeZyzzFan <lolvan@mail.ru>
2023-04-27 19:30:16 +0400

    Starting to develop dbcontext

---
+++ Recruitment/RecruitmentAgency/RecruitmentAgency/RecruitmentAgencyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RecruitmentAgency;
+public class RecruitmentAgencyDbContext : DbContext
+{
+    public DbSet<Company>? Companies { get; set; }
+    public DbSet<CompanyApplication>? CompanyApplications { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+    public DbSet<JobApplication>? JobApplications { get; set; }
+    public DbSet<Title>? Titles { get; set; } 
+
+    public RecruitmentAgencyDbContext(DbContextOptions options): base(options)
+    {
+        Database.EnsureCreated();
+    }
+}

8a7402c78216a4e55639edb5169e7ac04966b362
ad-klevtsov <ad.klevtsov@gmail.com>
2023-04-27 15:37:22 +0400

    Bike and BikeType classes created

---
+++ BikeRental/BikeRental.classes/BIke.cs
@@ -0,0 +1,22 @@
+﻿namespace BikeRental.classes;
+
+/// <summary>
+/// Class Bike has the info about bikes
+/// </summary>
+public class Bike
+{
+    /// <summary>
+    /// Serial number of a bike
+    /// </summary>
+    public int SerialNumber { get; set; }
+
+    /// <summary>
+    /// Model info of a bike
+    /// </summary>
+    public string Model { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Color info of a bike
+    /// </summary>
+    public string Color { get; set; } = string.Empty;
+}
---
+++ BikeRental/BikeRental.classes/BikeType.cs
@@ -0,0 +1,16 @@
+﻿namespace BikeRental.classes;
+/// <summary>
+/// Class BikeType has the info about bike types
+/// </summary>
+public class BikeType
+{
+    /// <summary>
+    /// Name of a bike type
+    /// </summary>
+    public string TypeName { get; set; } = string.Empty; 
+
+    /// <summary>
+    /// Price per hour of rent
+    /// </summary>
+    public int RentCost { get; set; }
+}

fc474c7cdf642a329ed59e9eccfcb71b1c2298e1
Sv163Sam <Tankruuus@gmail.com>
2023-05-04 11:52:38 +0400

    Init TaxiDepoClient project

---
+++ TaxiDepo.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TaxiDepo.Client.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ TaxiDepo.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace TaxiDepo.Client.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}

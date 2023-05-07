f5b876c2f2a0c81ea48eb5ac47a7605b8f15d8d9
YoniqueeZyzzFan <lolvan@mail.ru>
2023-05-07 17:45:06 +0400

    Created RecruitmentAgency.Client project Using avalonia 0.10.18

---
+++ Recruitment/RecruitmentAgency/RecruitmentAgency.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RecruitmentAgency.Client.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Recruitment/RecruitmentAgency/RecruitmentAgency.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace RecruitmentAgency.Client.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}

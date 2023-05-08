b36a80bd70ff902653a4fff2a778f830598a6e84
Admin <Admin@LAPTOP-U4DN8FG5>
2023-05-08 07:59:43 +0400

    Закончена работа над тестовыми данными

--- UniversityData/UniversityData.Domain/UniversityDataDbContext.cs
+++ UniversityData/UniversityData.Domain/UniversityDataDbContext.cs
@@ -4,10 +4,6 @@ using Microsoft.EntityFrameworkCore.Internal;
 namespace UniversityData.Domain;
 public class UniversityDataDbContext : DbContext
 {
-    public UniversityDataDbContext(DbContextOptions options)
-    {
-        Database.EnsureCreated();
-    }
     /// <summary>
     /// Коллекция объектов класса ConstructionProperty
     /// </summary>
@@ -41,35 +37,45 @@ public class UniversityDataDbContext : DbContext
     /// </summary>
     public DbSet<UniversityProperty> UniversityProperties { get; set; }
 
+    public UniversityDataDbContext(DbContextOptions options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<UniversityProperty>().HasData(
             new UniversityProperty
             {
                 Id = 1,
-                NameUniversityProperty = "мунициальная"
+                NameUniversityProperty = "муниципальная",
+                Universities= new List<University>()
             },
             new UniversityProperty
             {
                 Id = 2,
-                NameUniversityProperty = "частная"
+                NameUniversityProperty = "частная",
+                Universities = new List<University>()
             });
 
         modelBuilder.Entity<ConstructionProperty>().HasData(
             new ConstructionProperty
             {
                 Id = 1,
-                NameConstructionProperty = "муниципальная"
+                NameConstructionProperty = "муниципальная",
+                Universities = new List<University>()
             },
             new ConstructionProperty
             {
                 Id = 2,
-                NameConstructionProperty = "частная"
+                NameConstructionProperty = "частная",
+                Universities = new List<University>()
             },
             new ConstructionProperty
             {
                 Id = 3,
-                NameConstructionProperty = "федеральная"
+                NameConstructionProperty = "федеральная",
+                Universities = new List<University>()
             });
 
         modelBuilder.Entity<Specialty>().HasData(
@@ -77,31 +83,36 @@ public class UniversityDataDbContext : DbContext
             {
                 Id = 1,
                 Name = "Прикладная информатика",
-                Code = "09.03.03"
+                Code = "09.03.03",
+                SpecialtyTableNodes = new List<SpecialtyTableNode>()
             },
             new Specialty
             {
                 Id = 2,
                 Name = "Информационные системы и технологии",
-                Code = "09.03.02"
+                Code = "09.03.02",
+                SpecialtyTableNodes = new List<SpecialtyTableNode>()
             },
             new Specialty
             {
                 Id = 3,
                 Name = "Информатика и вычислительная техника",
-                Code = "09.03.01"
+                Code = "09.03.01",
+                SpecialtyTableNodes = new List<SpecialtyTableNode>()
             },
             new Specialty
             {
                 Id = 4,
                 Name = "Прикладная математика и информатика",
-                Code = "01.03.02"
+                Code = "01.03.02",
+                SpecialtyTableNodes = new List<SpecialtyTableNode>()
             },
             new Specialty
             {
                 Id = 5,
                 Name = "Информационная безопасность автоматизированных систем",
-                Code = "10.05.03"
+                Code = "10.05.03",
+                SpecialtyTableNodes = new List<SpecialtyTableNode>()
             });
 
         modelBuilder.Entity<Department>().HasData(
@@ -117,7 +128,7 @@ public class UniversityDataDbContext : DbContext
                 Id = 2,
                 Name = "Кафедры алгебры и геометрии",
                 SupervisorNumber = "890918735",
-                UniversityId = 1
+                UniversityId = 2
             },
             new Department
             {
@@ -288,14 +299,14 @@ public class UniversityDataDbContext : DbContext
                 Id = 10,
                 CountGroups = 8,
                 UniversityId = 2,
-                SpecialtyId = 6
+                SpecialtyId = 5
             },
             new SpecialtyTableNode
             {
                 Id = 11,
                 CountGroups = 8,
                 UniversityId = 3,
-                SpecialtyId = 7,
+                SpecialtyId = 1
             });
 
         modelBuilder.Entity<University>().HasData(
@@ -305,7 +316,12 @@ public class UniversityDataDbContext : DbContext
                Number = "12345",
                Name = "Самарский университет",
                Address = "Самара",
-               RectorId = 1
+               RectorId = 1,
+               UniversityPropertyId = 1,
+               ConstructionPropertyId = 1,
+               DepartmentsData = new List<Department>(),
+               FacultiesData = new List<Faculty>(),
+               SpecialtyTable = new List<SpecialtyTableNode>()
            },
            new University
            {
@@ -313,7 +329,12 @@ public class UniversityDataDbContext : DbContext
                Number = "56789",
                Name = "СамГТУ",
                Address = "Самара",
-               RectorId = 2
+               RectorId = 2,
+               UniversityPropertyId = 1,
+               ConstructionPropertyId = 1,
+               DepartmentsData = new List<Department>(),
+               FacultiesData = new List<Faculty>(),
+               SpecialtyTable = new List<SpecialtyTableNode>()
            },
            new University
            {
@@ -321,7 +342,12 @@ public class UniversityDataDbContext : DbContext
                Number = "45678",
                Name = "ПГУТИ",
                Address = "Самара",
-               RectorId = 2
-           });
+               RectorId = 3,
+               UniversityPropertyId = 1,
+               ConstructionPropertyId = 3,
+               DepartmentsData = new List<Department>(),
+               FacultiesData = new List<Faculty>(),
+               SpecialtyTable = new List<SpecialtyTableNode>()
+           }) ;
     }
 }

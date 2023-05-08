04ee8cdc04fabcc0d2f1fd7ac1433c4b1a542f40
ad-klevtsov <ad.klevtsov@gmail.com>
2023-05-08 20:46:42 +0400

    Fixture added

---
+++ BikeRental/BikeRental.Tests/BikeRentalFixture.cs
@@ -0,0 +1,59 @@
+﻿using BikeRental.Domain;
+
+namespace BikeRental.Tests;
+
+public class BikeRentalFixture
+{
+    private readonly List<BikeType> _bikeTypes;
+
+    private readonly List<Bike> _bikes;
+
+    private readonly List<Client> _clients;
+
+    private readonly List<RentRecord> _records;
+
+    public BikeRentalFixture()
+    {
+        _bikeTypes = new List<BikeType>
+        {
+             new BikeType { Id = 1, TypeName = "горный", RentCost = 300 },
+             new BikeType { Id = 1, TypeName = "прогулочный", RentCost = 100 },
+             new BikeType { Id = 1, TypeName = "спртивный", RentCost = 200 }
+        };
+
+        _bikes = new List<Bike>
+        {
+            new Bike { Id = 1, SerialNumber = 12345, Model = "Model1", Color = "green", Type = BikeTypes[0] },
+            new Bike { Id = 2, SerialNumber = 23451, Model = "Model2", Color = "green", Type = BikeTypes[1] },
+            new Bike { Id = 3, SerialNumber = 34512, Model = "Model3", Color = "green", Type = BikeTypes[2] },
+            new Bike { Id = 4, SerialNumber = 45123, Model = "Model4", Color = "green", Type = BikeTypes[0] },
+            new Bike { Id = 5, SerialNumber = 51234, Model = "Model5", Color = "green", Type = BikeTypes[2] }
+        };
+
+        _clients = new List<Client>
+        {
+            new Client { Id = 1, FullName = "Ivan Ivanov", BirthYear = 1995, PhoneNumber = "+7(927)123-45-67" },
+            new Client { Id = 2, FullName = "Petr Petrov", BirthYear = 1992, PhoneNumber = "+7(927)123-45-68" },
+            new Client { Id = 3, FullName = "Kuznec Kuznecov", BirthYear = 1986, PhoneNumber = "+7(927)123-45-69" },
+            new Client { Id = 4, FullName = "Andrey Andreev", BirthYear = 2000, PhoneNumber = "+7(927)123-45-60" },
+            new Client { Id = 4, FullName = "Ignat Ignatiev", BirthYear = 2001, PhoneNumber = "+7(927)123-45-61" }
+        };
+
+        _records = new List<RentRecord>
+        {
+            new RentRecord { Id = 1, ClientName = "Ivan Ivanov", BikeSerialNumber = 12345, RentStartTime = DateTime.Parse("2023-01-1 13:45"), RentEndTime = DateTime.Parse("2023-01-1 14:45") },
+            new RentRecord { Id = 2, ClientName = "Petr Petrov", BikeSerialNumber = 23451, RentStartTime = DateTime.Parse("2023-01-1 15:45"), RentEndTime = DateTime.Parse("2023-01-1 16:45") },
+            new RentRecord { Id = 3, ClientName = "Kuznec Kuznecov", BikeSerialNumber = 34512, RentStartTime = DateTime.Parse("2023-01-1 10:45"), RentEndTime = DateTime.Parse("2023-01-1 11:45") },
+            new RentRecord { Id = 4, ClientName = "Andrey Andreev", BikeSerialNumber = 45123, RentStartTime = DateTime.Parse("2023-01-1 8:45"), RentEndTime = DateTime.Parse("2023-01-1 9:45") },
+            new RentRecord { Id = 5, ClientName = "Ignat Ignatiev", BikeSerialNumber = 12345, RentStartTime = DateTime.Parse("2023-01-1 19:45"), RentEndTime = DateTime.Parse("2023-01-1 20:45") }
+        };
+    }
+
+    public List<BikeType> BikeTypes => _bikeTypes;
+
+    public List<Bike> Bikes => _bikes;
+
+    public List<Client> Clients => _clients;
+
+    public List<RentRecord> Records => _records;
+}
--- BikeRental/BikeRental.Tests/BikeRentalTests.cs
+++ BikeRental/BikeRental.Tests/BikeRentalTests.cs
@@ -3,33 +3,28 @@ using System.Threading.Tasks.Dataflow;
 
 namespace BikeRental.Tests;
 
-public class BikeRentalTests
+public class BikeRentalTests : IClassFixture<BikeRentalFixture>
 {
+    private readonly BikeRentalFixture _bikeRentalFixture;
+
+    /// <summary>
+    /// Constructor 
+    /// </summary>
+    /// <param name="bikeRentalFixture"></param>
+    public BikeRentalTests(BikeRentalFixture bikeRentalFixture)
+    {
+        _bikeRentalFixture = bikeRentalFixture;
+    }
+
     /// <summary>
     /// First request - give info about all sport bikes
     /// </summary>
     [Fact]
     public void TestSportBikes()
     {
-        var bikeTypes = new List<BikeType>
-        {
-            new BikeType{ Id = 1, TypeName = "горный", RentCost = 300 },
-            new BikeType{ Id = 1, TypeName = "прогулочный", RentCost = 100 },
-            new BikeType{ Id = 1, TypeName = "спртивный", RentCost = 200 }
-        };
-
-        var bikes = new List<Bike>
-        {
-            new Bike { Id = 1, SerialNumber = 12345, Model = "Model1", Color = "green", Type = bikeTypes[0] },
-            new Bike { Id = 2, SerialNumber = 23451, Model = "Model2", Color = "green", Type = bikeTypes[1] },
-            new Bike { Id = 3, SerialNumber = 34512, Model = "Model3", Color = "green", Type = bikeTypes[2] },
-            new Bike { Id = 4, SerialNumber = 45123, Model = "Model4", Color = "green", Type = bikeTypes[0] },
-            new Bike { Id = 5, SerialNumber = 51234, Model = "Model5", Color = "green", Type = bikeTypes[2] }
-        };
-
         var request = 
-            from bike in bikes
-            where bike.Type == bikeTypes[2]
+            from bike in _bikeRentalFixture.Bikes
+            where bike.Type == _bikeRentalFixture.BikeTypes[2]
             select bike;
 
         Assert.Equal(2, request.Count());
@@ -41,45 +36,11 @@ public class BikeRentalTests
     [Fact]
     public void TestMountainBikes()
     {
-        var bikeTypes = new List<BikeType>
-        {
-            new BikeType{ Id = 1, TypeName = "горный", RentCost = 300 },
-            new BikeType{ Id = 2, TypeName = "прогулочный", RentCost = 100 },
-            new BikeType{ Id = 3, TypeName = "спртивный", RentCost = 200 }
-        };
-
-        var bikes = new List<Bike>
-        {
-            new Bike { Id = 1, SerialNumber = 12345, Model = "Model1", Color = "green", Type = bikeTypes[0] },
-            new Bike { Id = 2, SerialNumber = 23451, Model = "Model2", Color = "green", Type = bikeTypes[1] },
-            new Bike { Id = 3, SerialNumber = 34512, Model = "Model3", Color = "green", Type = bikeTypes[2] },
-            new Bike { Id = 4, SerialNumber = 45123, Model = "Model4", Color = "green", Type = bikeTypes[0] },
-            new Bike { Id = 5, SerialNumber = 51234, Model = "Model5", Color = "green", Type = bikeTypes[2] }
-        };
-
-        var clients = new List<Client>
-        {
-            new Client { Id = 1, FullName = "Ivan Ivanov", BirthYear = 1995, PhoneNumber = "+7(927)123-45-67"},
-            new Client { Id = 2, FullName = "Petr Petrov", BirthYear = 1992, PhoneNumber = "+7(927)123-45-68"},
-            new Client { Id = 3, FullName = "Kuznec Kuznecov", BirthYear = 1986, PhoneNumber = "+7(927)123-45-69"},
-            new Client { Id = 4, FullName = "Andrey Andreev", BirthYear = 2000, PhoneNumber = "+7(927)123-45-60"},
-            new Client { Id = 4, FullName = "Ignat Ignatiev", BirthYear = 2001, PhoneNumber = "+7(927)123-45-61"}
-        };
-
-        var records = new List<RentRecord>
-        {
-            new RentRecord { Id = 1, ClientName = "Ivan Ivanov", BikeSerialNumber = 12345, RentStartTime= DateTime.Parse("2023-01-1 13:45"), RentEndTime= DateTime.Parse("2023-01-1 14:45") },
-            new RentRecord { Id = 2, ClientName = "Petr Petrov", BikeSerialNumber = 23451, RentStartTime= DateTime.Parse("2023-01-1 15:45"), RentEndTime= DateTime.Parse("2023-01-1 16:45") },
-            new RentRecord { Id = 3, ClientName = "Kuznec Kuznecov", BikeSerialNumber = 34512, RentStartTime= DateTime.Parse("2023-01-1 10:45"), RentEndTime= DateTime.Parse("2023-01-1 11:45") },
-            new RentRecord { Id = 4, ClientName = "Andrey Andreev", BikeSerialNumber = 45123, RentStartTime= DateTime.Parse("2023-01-1 8:45"), RentEndTime= DateTime.Parse("2023-01-1 9:45") },
-            new RentRecord { Id = 5, ClientName = "Ignat Ignatiev", BikeSerialNumber = 12345, RentStartTime= DateTime.Parse("2023-01-1 19:45"), RentEndTime= DateTime.Parse("2023-01-1 20:45") }
-        };
-
         var request = 
-            from client in clients
-            join record in records on client.FullName equals record.ClientName
-            join bike in bikes on record.BikeSerialNumber equals bike.SerialNumber
-            where bike.Type == bikeTypes[0]
+            from client in _bikeRentalFixture.Clients
+            join record in _bikeRentalFixture.Records on client.FullName equals record.ClientName
+            join bike in _bikeRentalFixture.Bikes on record.BikeSerialNumber equals bike.SerialNumber
+            where bike.Type == _bikeRentalFixture.BikeTypes[0]
             orderby client.FullName 
             select client;
 

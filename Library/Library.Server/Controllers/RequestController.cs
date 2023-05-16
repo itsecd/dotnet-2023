11d8341fb283deec39ed14f11ac9f81b1a686947
Влад Черный <stalkervaultboy@gmail.com>
2023-05-17 01:37:21 +0400

    Renamed parameter in lambda expression in second request in RequestController

--- Library/Library.Server/Controllers/RequestController.cs
+++ Library/Library.Server/Controllers/RequestController.cs
@@ -72,15 +72,15 @@ public class RequestController : ControllerBase
                                 }).ToListAsync();
         var request = (from req in bookSelect
                        orderby req.book.Name
-                       select req).Select(b => new BookGetDto
+                       select req).Select(book => new BookGetDto
                        {
-                           Id = b.book.Id,
-                           Author = b.book.Author,
-                           Cipher = b.book.Cipher,
-                           Name = b.book.Name,
-                           PlaceEdition = b.book.PlaceEdition,
-                           YearEdition = b.book.YearEdition,
-                           TypeEditionId = b.book.TypeEditionId,
+                           Id = book.book.Id,
+                           Author = book.book.Author,
+                           Cipher = book.book.Cipher,
+                           Name = book.book.Name,
+                           PlaceEdition = book.book.PlaceEdition,
+                           YearEdition = book.book.YearEdition,
+                           TypeEditionId = book.book.TypeEditionId,
                        }).ToList();
         if (request.Count == 0)
         {

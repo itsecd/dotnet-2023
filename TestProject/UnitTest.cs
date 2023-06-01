a68ae56ba77afc748b38654ea7c5592a5c857545
Alena-Oleynichenko <alena.ryabova12@gmail.com>
2023-06-01 21:39:36 +0400

    rechecked the data to check the operation of unit tests, fixed errors that appeared on the background of changing data when filling out the fixtureRealt

--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -35,21 +35,21 @@ public class UnitTest : IClassFixture<FixtureRealt>
                               grp.Key.Passport
                           }).ToList(); ;
         Assert.Equal(3, resultList.Count);
-        Assert.Equal("Gorshnev", resultList[0].Surname);
-        Assert.Equal("Michael", resultList[0].Name);
-        Assert.Equal("Aerodromnaya, 47a", resultList[0].Registration);
-        Assert.Equal("880-05-55", resultList[0].Number);
-        Assert.Equal("91245", resultList[0].Passport);
+        Assert.Equal("Shnirov", resultList[0].Surname);
+        Assert.Equal("Sergey", resultList[0].Name);
+        Assert.Equal("Krasnoarmeyskaya, 1a", resultList[0].Registration);
+        Assert.Equal("336-64-36", resultList[0].Number);
+        Assert.Equal("32218", resultList[0].Passport);
         Assert.Equal("Paramonov", resultList[1].Surname);
         Assert.Equal("Ilya", resultList[1].Name);
         Assert.Equal("Galaktionovskaya, 40", resultList[1].Registration);
         Assert.Equal("341-21-09", resultList[1].Number);
         Assert.Equal("21415", resultList[1].Passport);
-        Assert.Equal("Shnirov", resultList[2].Surname);
-        Assert.Equal("Sergey", resultList[2].Name);
-        Assert.Equal("Krasnoarmeyskaya, 1a", resultList[2].Registration);
-        Assert.Equal("336-64-36", resultList[2].Number);
-        Assert.Equal("32218", resultList[2].Passport);
+        Assert.Equal("Gorshnev", resultList[2].Surname);
+        Assert.Equal("Michael", resultList[2].Name);
+        Assert.Equal("Aerodromnaya, 47a", resultList[2].Registration);
+        Assert.Equal("880-05-55", resultList[2].Number);
+        Assert.Equal("91245", resultList[2].Passport);
     }
     /// <summary>
     /// second request - all sellers who left orders for a given period
@@ -133,7 +133,7 @@ public class UnitTest : IClassFixture<FixtureRealt>
                       join house in _fixture.Houses on connect.HouseId equals house.Id
                       where house.Type == "Residential"
                       select applications).Count();
-        Assert.Equal(5, query1);
+        Assert.Equal(6, query1);
         Assert.Equal(2, query2);
     }
     /// <summary>
@@ -161,9 +161,9 @@ public class UnitTest : IClassFixture<FixtureRealt>
                       orderby query1.Count descending
                       select client).Take(5).ToList();
         Assert.Equal(4, result.Count());
-        Assert.Equal(1, result[0].Count);
-        Assert.Equal("Gorshnev", result[0].Surname);
-        Assert.Equal("Terrible", result[3].Surname);
+        Assert.Equal(0, result[0].Count);
+        Assert.Equal("Shnirov", result[0].Surname);
+        Assert.Equal("Gorshnev", result[3].Surname);
         var query2 = (from clien in _fixture.Clients
                       join applications in _fixture.Applications on clien.Id equals applications.ClientId
                       where applications.Type == "Sale"
@@ -183,7 +183,7 @@ public class UnitTest : IClassFixture<FixtureRealt>
                        orderby query2.Count descending
                        select client).Take(5).ToList();
         Assert.Single(result2);
-        Assert.Equal(1, result2[0].Count);
+        Assert.Equal(0, result2[0].Count);
         Assert.Equal("King", result2[0].Surname);
     }
     /// <summary>

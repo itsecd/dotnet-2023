b7d7fce5a89823c0cf7e2ecc292ec5644c1867e8
Нгок Хунг Фам <2020-03729@students.ssau.ru>
2023-07-02 00:07:33 +0400

    Update 2 - issue

--- LibrarySchool.Server/Program.cs
+++ LibrarySchool.Server/Program.cs
@@ -1,19 +1,12 @@
 using AutoMapper;
+using FluentValidation;
 using LibrarySchool.Domain;
+using LibrarySchool.Server.Dto.Validator;
 using LibrarySchoolServer;
-<<<<<<< HEAD
 using LibrarySchoolServer.Dto;
-=======
-using Microsoft.AspNetCore.Builder;
->>>>>>> 2-dto-classes-to-records
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
-<<<<<<< HEAD
 
-// 3 issue
-=======
-//2 - issue
->>>>>>> 2-dto-classes-to-records
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -24,14 +17,13 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
 
 });
-<<<<<<< HEAD
+
 builder.Services.AddScoped<IValidator<StudentPostDto>, StudentPostDtoValidator>();
 builder.Services.AddScoped<IValidator<ClassTypePostDto>, ClassTypePostDtoValidator>();
 builder.Services.AddScoped<IValidator<SubjectPostDto>, SubjectPostDtoValidator>();
 builder.Services.AddScoped<IValidator<MarkPostDto>, MarkPostDtoValidator>();
-=======
 
->>>>>>> 2-dto-classes-to-records
+
 builder.Services.AddDbContextFactory<LibrarySchoolContext>(optionsBuilder =>
 {
     var connectionString = builder.Configuration.GetConnectionString(nameof(LibrarySchool));

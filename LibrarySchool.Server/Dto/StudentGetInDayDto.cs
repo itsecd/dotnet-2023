14bad01af4c929bfcce7e70d9a640b420f097336
Нгок Хунг Фам <2020-03729@students.ssau.ru>
2023-07-01 23:36:47 +0400

    Add new record

---
+++ LibrarySchool.Server/Dto/StudentGetInDayDto.cs
@@ -0,0 +1,15 @@
+﻿namespace LibrarySchool.Server.Dto;
+
+/// <summary>
+/// DtoClassOfStudent who get mark in day
+/// </summary>
+/// <param name="StudentId"></param>
+/// <param name="StudentName"></param>
+/// <param name="SubjectId"></param>
+/// <param name="SubjectName"></param>
+/// <param name="MarkValue"></param>
+public record StudentGetInDayDto(int StudentId,
+                                 string StudentName,
+                                 int SubjectId,
+                                 int SubjectName,
+                                 int MarkValue);
